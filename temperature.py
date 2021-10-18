a216eac909f85822a57a0c912acaee910426c0a0
aohengyang <yykk662@gmail.com>
2021-10-18 16:34:49 -0400

    temperature both html and python version

---
+++ temperature.py
@@ -0,0 +1,6 @@
+
+tempincelsius = input("please enter a temperature in Celsius")
+tempincelsius = float(tempincelsius)
+tempinfahrenheit = tempincelsius * 1.8 + 32
+tempinfahrenheit = round(tempinfahrenheit, 2)
+print(str(tempincelsius) + " degrees Celsius is " + str(tempinfahrenheit) +" degrees Fahrenheit. ")
