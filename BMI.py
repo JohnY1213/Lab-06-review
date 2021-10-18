b842c78890613dcb4e1164385bb6ae8c0f177909
aohengyang <yykk662@gmail.com>
2021-10-18 17:10:21 -0400

    finish mile and BMI

--- BMI.py
+++ BMI.py
@@ -4,6 +4,9 @@ print("BMI: Body Mass Index Calculator v.0.1 beta")
 userWeight = input("Enter your weight (in pound): ")
 userHeight = input("Enter your Height (in inches): ")
 
+
+#myBMI = (703 * float(userWeight)) /(float(userHeight)*float(userHeight))
+
 myBMI = (703 * float(userWeight)) / pow(float(userHeight),2)
-myBMI =round(myBMI,2)
+myBMI =round(myBMI, 2)
 print("you body mass index (BMI) is " + str(myBMI) + "%")
